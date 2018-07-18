c5c7689edeb514dd01ef79a6d10b610afd25ac0c
onratlgn <onratlgn@gmail.com>
2018-07-18 14:14:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,14 @@ var ws = require('websocket-stream')
 var mqPort = 1883
 var wsPort = 8888
 
+/*
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
+*/
 
 server.listen(mqPort, () => console.log('mqtt listening'))
 ws.createServer({
